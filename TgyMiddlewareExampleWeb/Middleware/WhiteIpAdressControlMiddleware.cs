8400bea1c2ce1df41d8bbd5f68cb63f1b904d7a5
Ercan Enes KARACA <karaca_58ercan@hotmail.com>
2023-11-02 21:40:45 +0300

    4. Ders 3.Bölüm

---
+++ TgyMiddlewareExampleWeb/Middleware/WhiteIpAdressControlMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+
+namespace TgyMiddlewareExampleWeb.Middleware
+{
+    public class WhiteIpAdressControlMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        private const string WhiteIpAdress = "::2";
+
+        public WhiteIpAdressControlMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //IPv4 -> 127.0.01 -> localhost
+            //IPv6 -> ::1 -> localhost
+
+            var reqIpAdress = context.Connection.RemoteIpAddress;
+
+            bool anyWhiteIpAdress = IPAddress.Parse(WhiteIpAdress).Equals(reqIpAdress);
+            
+            if (anyWhiteIpAdress)
+            {
+                await _requestDelegate(context);
+            }
+            else
+            {
+                context.Response.StatusCode =HttpStatusCode.Forbidden.GetHashCode();
+                await context.Response.WriteAsync("Forbidden");
+            }
+        }
+    }
+}
---
+++ TgyMiddlewareExampleWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TgyMiddlewareExampleWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
